734f77470fd84bc534b53be521a90dea569ce51a
shashi <shashikumar3089@gmail.com>
2024-05-08 19:20:54 +0530

    strems example

---
+++ Java/Java_8/Streams/src/com/skoti/foreach/employee/Employee.java
@@ -0,0 +1,64 @@
+package com.skoti.foreach.employee;
+
+public class Employee {
+
+  private int empId;
+  private String name;
+  private String dept;
+  private Double salary;
+
+  public Employee(int empId, String name, String dept, Double salary) {
+    this.empId = empId;
+    this.name = name;
+    this.dept = dept;
+    this.salary = salary;
+  }
+
+  public int getEmpId() {
+    return empId;
+  }
+
+  public void setEmpId(int empId) {
+    this.empId = empId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDept() {
+    return dept;
+  }
+
+  public void setDept(String dept) {
+    this.dept = dept;
+  }
+
+  public Double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(Double salary) {
+    this.salary = salary;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{"
+        + "empId="
+        + empId
+        + ", name='"
+        + name
+        + '\''
+        + ", dept='"
+        + dept
+        + '\''
+        + ", salary="
+        + salary
+        + '}';
+  }
+}
