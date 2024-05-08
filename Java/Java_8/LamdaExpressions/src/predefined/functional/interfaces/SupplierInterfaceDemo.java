0ab50559305931f79964554b28d388dc2046d498
shashi <shashikumar3089@gmail.com>
2024-05-08 18:20:09 +0530

    consumer predicate supplier

--- Java/Java_8/LamdaExpressions/src/predefined/functional/interfaces/SupplierInterfaceDemo.java
+++ Java/Java_8/LamdaExpressions/src/predefined/functional/interfaces/SupplierInterfaceDemo.java
@@ -10,12 +10,13 @@ public class SupplierInterfaceDemo {
     Supplier<String> supplier = () -> "Output from Supplier";
     System.out.println(supplier.get());
 
-    // filter in streams uses Supplier
+    // findAny in streams uses Supplier
     List<String> stringList = Arrays.asList();
     System.out.println(stringList.stream().findAny().orElseGet(supplier));
 
     // refactoring above code
     List<String> stringList2 = Arrays.asList();
     System.out.println(stringList2.stream().findAny().orElseGet( () -> "Output from SupplierLambda"));
+
   }
 }
