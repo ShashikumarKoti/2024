d207459f9d1e0ab71ba8eda64dee039fc3dc8061
Shashi Kumar S Koti <shashiko@maveric-systems.com>
2024-06-21 12:49:48 +0530

    template driven and reactive form

--- Angular/typeScriptForAngular/src/app/app.component.ts
+++ Angular/typeScriptForAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,35 @@ export class AppComponent {
     age: 35
   }
 
+  loginFormFromReactive=new FormGroup({
+    userName:new FormControl('', [Validators.required,Validators.pattern('[a-zA-z]+$')]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)]),
+    email:new FormControl('', [Validators.email])
+
+  })
+
   convertStringToUpperCase(item:string){
    return  item.toUpperCase();
   }
 
+  userLogin(item: any) {
+    console.warn(item)
+  }
+
+  loginUserFromReactive() {
+    console.warn(this.loginFormFromReactive.value);
+  }
+
+  get userValidator() {
+    return this.loginFormFromReactive.get('userName')
+  }
+
+  get emailValidator() {
+    return this.loginFormFromReactive.get('email')
+  }
+
+  get pwdValidator() {
+    return this.loginFormFromReactive.get('password')
+  }
 
 }
--- Angular/typeScriptForAngular/src/app/app.module.ts
+++ Angular/typeScriptForAngular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UsdInrPipe } from './pipes/usd-inr.pipe';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component'
+import { ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsdInrPipe
+    UsdInrPipe,
+    TemplateDrivenFormComponent,
+    ReactiveFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
