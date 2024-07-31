1a97bca14ca04b7802ef71331f34fc9c92cbeb1d
shashiko <shashikimar3089@gmail.com>
2024-07-31 16:22:17 +0530

    junit practice

--- SpringBoot/SB_Junit/src/test/java/com/example/skoti/MyMathTest.java
+++ SpringBoot/SB_Junit/src/test/java/com/example/skoti/MyMathTest.java
@@ -1,47 +1,46 @@
 package com.example.skoti;
 
 import com.example.skoti.service.MyMath;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MyMathTest {
+class MyMathTest {
 
     MyMath myMath = new MyMath();
 
-    @Before
+    @BeforeEach
     public void before() {
         System.out.println("Before");
     }
 
-    @After
+    @AfterEach
     public void after() {
         System.out.println("After");
     }
 
-    @BeforeClass
+    @BeforeAll
     public static void beforeClass() {
         System.out.println("Before Class");
     }
 
 
-    @AfterClass
+    @AfterAll
     public static void afterClass() {
         System.out.println("After Class");
     }
 
     @Test
-    public void sum_with3numbers() {
+    void sum_with3numbers() {
         System.out.println("Test1");
         assertEquals(6, myMath.sum(new int[] { 1, 2, 3 }));
     }
 
     @Test
-    public void sum_with1number() {
+    void sum_with1number() {
         System.out.println("Test2");
         assertEquals(3, myMath.sum(new int[] { 3 }));
     }
