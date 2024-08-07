225d1b5f0cbb9cd5f5f317709a0efc3588cbe22c
shashi <shashikumar3089@gmail.com>
2024-08-07 23:31:06 +0530

    primeNumbers

--- Java/Java_8/GeneralPrograms/src/com/skoti/programs/numbers/PrimeNumberSeries.java
+++ Java/Java_8/GeneralPrograms/src/com/skoti/programs/numbers/PrimeNumberSeries.java
@@ -10,11 +10,44 @@ public class PrimeNumberSeries {
 
         int number = 20;
         List<Integer> primeNumbersList = IntStream.rangeClosed(2, number).
-                filter(PrimeNumberSeries::isPrime).boxed().collect(Collectors.toList());
+                filter(PrimeNumberSeries::primeNumberSeriesUsingJava8).boxed().collect(Collectors.toList());
         System.out.println(primeNumbersList);
+        checkPrime(19);
+        primeNumberSeries(20);
     }
 
-    public static boolean isPrime(int number) {
-        return IntStream.rangeClosed(2, (int) Math.sqrt(number)).allMatch(i-> number % i != 0);
+    private static void primeNumberSeries(int series) {
+        for (int i=2;i<=series;i++) {
+            int temp = 0;
+            for (int j=2;j<i;j++) {
+                if (i%j==0) {
+                    temp =1;
+                    break;
+                }
+            }
+            if (temp == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    private static void checkPrime(int num) {
+        int temp = 0;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.println("Given num " + num + " is not prime");
+                temp = 1;
+                break;
+            }
+        }
+        if (temp == 0) {
+            System.out.println("Given num " + num + " is a prime");
+        }
+    }
+
+    public static boolean primeNumberSeriesUsingJava8(int number) {
+        //  return IntStream.rangeClosed(2, (number/2)+1).allMatch( num -> number%2 !=0);
+        return IntStream.rangeClosed(2, (int) Math.sqrt(number)).allMatch(i -> number % i != 0);
+
     }
 }
