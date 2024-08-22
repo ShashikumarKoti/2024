36cce87a4d38f500701913f3e7e31976623372f7
shashiko <shashikimar3089@gmail.com>
2024-08-22 18:39:19 +0530

    spring security ldap

---
+++ SpringBoot/SB_Security_LDAP/src/main/java/com/example/demo/SbSecurityLDAPApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityLDAPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityLDAPApplication.class, args);
+	}
+
+}
