733cc198a332346ffc5cfeeb27c090ad7ed3966d
shashiko <shashikimar3089@gmail.com>
2024-08-22 23:39:11 +0530

    spring security oauth

---
+++ SpringBoot/SB_Security_OAuth/src/main/java/com/example/demo/config/SecurityConfigUsingAuthorization.java
@@ -0,0 +1,56 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+@EnableWebSecurity
+public class SecurityConfigUsingAuthorization extends WebSecurityConfigurerAdapter {
+
+    //by default this datasource is pointing to h2 database.
+    @Autowired
+    private DataSource dataSource;
+
+    //Approach-1: to use default schema
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+//        //set your configuration on the auth object
+//        //When we are using embedded database, spring-boot autoconfigures the datasource
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource);
+//    }
+
+    //Approach-2: to use different schema instead of default schema
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        //set your configuration on the auth object
+        //When we are using embedded database, spring-boot autoconfigures the datasource
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled from my_users where username=?")
+                .authoritiesByUsernameQuery("select username,authority from my_authorities where username=?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+            http.authorizeRequests()
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/").permitAll()
+                    .and()
+                    .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ SpringBoot/SB_Security_OAuth/src/main/java/com/example/demo/controller/ResourceController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/")
+    public String home() {
+        return "No Authentication";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Welcome Admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Welcome User";
+    }
+}
